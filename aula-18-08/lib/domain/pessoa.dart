2486783c25130808b40175b852c9b03ff5394fb1
Allan Ogawa <allanogawa@gmail.com>
2023-08-18 21:13:51 -0300

    a

--- aula-18-08/lib/domain/pessoa.dart
+++ aula-18-08/lib/domain/pessoa.dart
@@ -1,3 +1,18 @@
 class Pessoa {
+  
+  String? nome;
+  int? idade;
 
+  Pessoa(this.nome, this.idade);
+
+  /* devemos criar o construtor para chamar esta classe e atribuir valores */
+  /* 1. aceita somente o NOME */
+  Pessoa.construtorNome(this.nome);
+
+  /* 2. aceita idade e nome independe ta ordem -> para construir deve-se passar o var a ser definido e o seu valor */
+  Pessoa.total({this.idade, this.nome});
+
+  String? getNome() {
+    return nome;
+  }
 }
--- aula-18-08/lib/poo.dart
+++ aula-18-08/lib/poo.dart
@@ -1,3 +1,17 @@
-int calculate() {
-  return 6 * 7;
+import 'package:aula_18_08/domain/pessoa.dart';
+
+void main(List<String> arguments) {
+
+  Pessoa eduardo = Pessoa('Eduardo', 20);
+  print(eduardo.nome);
+
+  Pessoa maria = Pessoa.construtorNome("Maria");
+  print(maria.nome);
+
+  Pessoa fabio = Pessoa.construtorNome("Fabio");
+  print(fabio.nome);
+
+  Pessoa teste = Pessoa.total(idade: 30, nome: "Richard");
+  print(teste.nome);
+
 }
